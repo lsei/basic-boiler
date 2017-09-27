0347d731bbf8440a6ee45099ef1fdc1648e1e394
Lucas <hello@lucasseidenfaden.com>
2017-09-27 15:22:07 +0100

    add testing utils

---
+++ test/helpers.js
@@ -0,0 +1,16 @@
+
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { spy } from 'sinon';
+import { configure, mount, render, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+
+configure({adapter: new Adapter() })
+
+global.expect = expect;
+global.sinon = sinon;
+global.spy = spy;
+
+global.mount = mount;
+global.render = render;
+global.shallow = shallow;
